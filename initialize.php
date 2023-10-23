fcd168085dc6afeda3f8c1c63e36e1cd7eaddc13
Karan9409632749 <Parmark1258@gmail.com>
2023-10-23 09:10:57 +0530

    init commit

---
+++ initialize.php
@@ -0,0 +1,10 @@
+<?php
+$dev_data = array('id'=>'-1','firstname'=>'Developer','lastname'=>'','username'=>'dev_oretnom','password'=>'5da283a2d990e8d8512cf967df5bc0d0','last_login'=>'','date_updated'=>'','date_added'=>'');
+if(!defined('base_url')) define('base_url','karantestapp1994.azurewebsites.net/');
+if(!defined('base_app')) define('base_app', str_replace('\\','/',__DIR__).'/' );
+if(!defined('dev_data')) define('dev_data',$dev_data);
+if(!defined('DB_SERVER')) define('DB_SERVER',"karantestapp1994-server2.mysql.database.azure.com");
+if(!defined('DB_USERNAME')) define('DB_USERNAME',"karan");
+if(!defined('DB_PASSWORD')) define('DB_PASSWORD',"Root@123");
+if(!defined('DB_NAME')) define('DB_NAME',"mobile_store_db");
+?>
